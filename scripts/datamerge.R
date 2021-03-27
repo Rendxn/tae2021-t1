b6cccc2c74adef2e40e2350a6c64685a2b041431
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2021-03-27 16:00:29 -0500

    Added comments to datamerge.R

--- scripts/datamerge.R
+++ scripts/datamerge.R
@@ -1,25 +1,32 @@
 library(dplyr)
 
+# All files with directory
 file_paths <-
   list.files(path = './data',
              pattern = '*.csv',
-             full.names = TRUE) # All files with directory
+             full.names = TRUE)
 
+# All file names (without directory)
 file_names <-
   list.files(path = './data',
-             pattern = '*.csv') # All file names (without directory)
+             pattern = '*.csv')
 
+# Cols that will be passed in the full_join
 merge_cols <- c('ï..DIRECTORIO', 'SECUENCIA_ENCUESTA')
 # , 'SECUENCIA_ENCUESTA', 'SECUENCIA_P', 'ORDEN', 'FEX_C'
 
+# Read all files and store each one in a separate data frame
 files <- lapply(
   file_paths,
   read.table,
   header = TRUE,
   sep = ';',
   dec = ','
-) # Read all files and store each one in a separate data frame
+)
 
+# Function that returns the full join
+# of two given dataframes by
+# `merge_cols`
 multiple_full <- function(...){
   df1 = list(...)[[1]]
   df2 = list(...)[[2]]
@@ -27,9 +34,12 @@ multiple_full <- function(...){
   return(joined)
 }
 
-all_df <- Reduce(multiple_full, files) # Full join all of the data frames
-names(all_df)[1] = 'DIRECTORIO' # Rename from ï..DIRECTORIO to DIRECTORIO
+# Full join all of the data frames using Reduce.
+all_df <- Reduce(multiple_full, files)
+# Rename from ï..DIRECTORIO to DIRECTORIO
+names(all_df)[1] = 'DIRECTORIO'
 
+# Columns to be filtered from the whole DB
 filtered_col_names <-
   c(
     'DIRECTORIO',
